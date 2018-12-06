7ac939b517751e09cc4285e7084e7e244983876a
Rafael Sampaio Tavares <rafael_killua@msn.com>
2018-12-06 12:08:27 -0300

    Trocando config e configurando ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,17 @@
 module.exports = {
-    plugins: ["vue"], // enable vue plugin
-    extends: ["plugin:vue/essential", "prettier"] // activate vue related rules
+    env: {
+        browser: true,
+        commonjs: true,
+        es6: true
+    },
+    extends: ["eslint:recommended", "plugin:vue/essential", "prettier"],
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: "module"
+    },
+    rules: {
+        indent: ["warn", 4],
+        quotes: ["warn", "double"],
+        semi: ["warn", "always"]
+    }
 };
--- services/fireinit.js
+++ services/fireinit.js
@@ -3,15 +3,10 @@ import "firebase/auth";
 import "firebase/database";
 import "firebase/storage";
 
+import firebaseConfig from "./firebaseConfig.json";
+
 if (firebase.apps.length < 1)
-    firebase.initializeApp({
-        apiKey: "AIzaSyACfeKhAs8D2XOpxduJSfbZ1c9dpOFpPow",
-        authDomain: "pets-2650.firebaseapp.com",
-        databaseURL: "https://pets-2650.firebaseio.com",
-        projectId: "pets-2650",
-        storageBucket: "pets-2650.appspot.com",
-        messagingSenderId: "679136849663"
-    });
+    firebase.initializeApp(firebaseConfig);
 
 export const auth = firebase.auth();
 export const db = firebase.database();
